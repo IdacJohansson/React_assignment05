e183608c17a8ad441ad1d95b79c702a6077be983
Ida Johansson <ida.johansson90@hotmail.com>
2024-12-03 10:54:29 +0100

    Added: search-bar + options-list

--- app/src/pages/Seker.jsx
+++ app/src/pages/Seker.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 
+import searchIcon from "../assets/search.svg";
+
 function Seker() {
   return (
     <main>
-      <div>
-        <p>MAIN</p>
+      <div className="search-container">
+        <div>
+          <img className="search-icon" src={searchIcon} alt="Search Icon" />
+        </div>
+        <input
+          className="search-field"
+          type="search"
+          id="search-field"
+          placeholder="....."
+        />
+        <button className="search-btn" id="search-btn">
+          Search
+        </button>
+      </div>
+      <div className="option-container">
+        <ul className="options-ul">
+          <li>Hej</li>
+          <li>Hej</li>
+          <li>Hej</li>
+        </ul>
       </div>
     </main>
   );
